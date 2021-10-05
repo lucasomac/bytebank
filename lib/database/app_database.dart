2def03a0cd3767f7b456a69df05b1491a2806f6a
Lucas de Oliveira Macedo <lucasomac@santander.com.br>
2021-10-05 17:16:36 -0300

    Fixed: use asynca and await

--- lib/database/app_database.dart
+++ lib/database/app_database.dart
@@ -2,35 +2,51 @@ import 'package:bytebank/models/contact.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
-Future<Database> createDatabase() {
-  return getDatabasesPath().then((dbPath) {
-    final String path = join(dbPath, 'bytebank.db');
-    return openDatabase(
-      path,
-      onCreate: (db, version) {
-        db.execute(
-            'CREATE TABLE contacts(id INTEGER PRIMARY KEY, name TEXT, accountNumber INTEGER)');
-      },
-      version: 1,
-      onDowngrade: onDatabaseDowngradeDelete,
-    );
-  });
+Future<Database> getDatabase() async {
+  final String path = join(await getDatabasesPath(), 'bytebank.db');
+  return openDatabase(
+    path,
+    onCreate: (db, version) {
+      db.execute(
+          'CREATE TABLE contacts(id INTEGER PRIMARY KEY, name TEXT, accountNumber INTEGER)');
+    },
+    version: 1,
+    onDowngrade: onDatabaseDowngradeDelete,
+  );
+
+  // return getDatabasesPath().then((dbPath) {
+  //   final String path = join(dbPath, 'bytebank.db');
+  //   return openDatabase(
+  //     path,
+  //     onCreate: (db, version) {
+  //       db.execute(
+  //           'CREATE TABLE contacts(id INTEGER PRIMARY KEY, name TEXT, accountNumber INTEGER)');
+  //     },
+  //     version: 1,
+  //     onDowngrade: onDatabaseDowngradeDelete,
+  //   );
+  // });
 }
 
-Future<int> save(Contact contact) {
-  return createDatabase().then((db) {
-    return db.insert('contacts', contact.toJson());
-  });
+Future<int> save(Contact contact) async {
+  final Database db = await getDatabase();
+  return db.insert('contacts', contact.toJson());
 }
 
-Future<List<Contact>> findAll() {
-  return createDatabase().then((db) {
-    return db.query('contacts').then((data) {
-      List<Contact> lista = [];
-      for (var element in data) {
-        lista.add(Contact.fromJson(element));
-      }
-      return lista;
-    });
-  });
+Future<List<Contact>> findAll() async {
+  final Database db = await getDatabase();
+  final List<Map<String, dynamic>> results = await db.query('contacts');
+  List<Contact> lista = [];
+  for (var element in results) {
+    lista.add(Contact.fromJson(element));
+  }
+  return lista;
+
+  // return db.query('contacts').then((data) {
+  //   List<Contact> lista = [];
+  //   for (var element in data) {
+  //     lista.add(Contact.fromJson(element));
+  //   }
+  //   return lista;
+  // });
 }
